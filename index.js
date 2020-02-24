33b1de2685f4649253756d8afcb2ce0fba75549c
Rafael <rafaelcocone@gmail.com>
2020-02-24 13:06:55 -0600

    chat 14-02-2020

--- index.js
+++ index.js
@@ -1,145 +1,196 @@
-var fs = require('fs')
-var https = require('https')
-const mysql = require('mysql')
-const express = require('express');
-const app = express();
+/*'use strict'
+
+var fs = require('fs'),
+    puerto = 3000,
+    https = require('https');
+var mensajero = require('./mysqlConnector');
+const mysql = require('mysql'),
+      express = require('express'),
+      app = express(),
+      options = {
+        key:  fs.readFileSync('key.pem'),
+        cert: fs.readFileSync('cert.pem')
+      };
 //inicializa el servoidor
 //settieng
-app.set('port', process.env.PORT || 3000);
-const mensajero = mysql.createConnection({
-  host: '74.208.166.96',
-  user: 'asys',
-  password: '1nt3gr4*2019',
-  port: 5543,
-  database: 'simulador'
-});
-const options = {
-  key:  fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
-
-const server = https.createServer(options, app);
+app.set('port', process.env.PORT || puerto);
 //websocket
-const SocketIO = require('socket.io');
-const io = SocketIO(server);
+const server = https.createServer(options, app),
+      SocketIO = require('socket.io');
+const  io = SocketIO(server);
 server.listen(app.get('port'), () => { console.log("server port",app.get('port')) });
 
-var roomAbiertas = [ ];
-var usuariosActivos = [ ];
+*/
 
-//coneccion a la base de datos
-mensajero.connect(function(error){
-    if(error){
-       throw error;
-    }else{
-       console.log('Conexion correcta.');
-    }
-  });
-    
-io.on('connection', (socket) => {
-    var item = {},
-        _id_doom = "",
-        _user = "",
-        _username = "";
+var fs = require('fs'),
+    puerto = 3000,
+    https = require('https');
+const mysql = require('mysql'),
+      express = require('express'),
+      app = express(),
+      mensajero = mysql.createConnection({
+        host: '74.208.166.96',
+        user: 'asys',
+        password: '1nt3gr4*2019',
+        port: 5543,
+        database: 'simulador'
+      }),
+      options = {
+        key:  fs.readFileSync('key.pem'),
+        cert: fs.readFileSync('cert.pem')
+      };
+//inicializa el servoidor
+//settieng
+app.set('port', process.env.PORT || puerto);
+//websocket
+const server = https.createServer(options, app),
+      SocketIO = require('socket.io');
+const  io = SocketIO(server);
+server.listen(app.get('port'), () => { console.log("server port",app.get('port')) });
 
-    socket.on('new message', (data) => {
-      // we tell the client to execute 'new message'
-      socket.to('chat-'+data.id_room).emit('new message', {//socket.broadcast.emit('new message', {//
-        username: data.username,
-        message:  data.message, 
-        id_room:  data.id_room, 
-        user:     data.user, 
-      });
-      var sql = "INSERT INTO comunicacionDtRoomMensajes (mensaje,state, id_comunicacionCtRoom, id_users_envio) VALUES ?";
-      var values = [
-        [data.message, 'A',data.id_room,data.user]
-      ];
-      mensajero.query(sql, [values], function (err, result) {
-        if (err){
-          console.log(err);
-         
-        } 
-      });
-
+var promise = new Promise(function (resolve, reject){
+    //coneccion a la base de datos
+    mensajero.connect(function(err){
+      return (err) ? reject ( new Error('Error en la coneccion de la base de datos notifer') ) :resolve (mensajero)
     });
-  
-    // when the client emits 'add user', this listens and executes
-    socket.on('add user', (data) => {
-      socket.join('chat-'+data.id_room, () => {
-        _id_doom  = data.id_room;
-        _user     = data.id_origen;
-        _username = data.username;
-        let resultados = [];
-  
-      let id_room = data.id_room;
-      let participantes = 0//addRoom(data.id_room)
-     
-          io.in('chat-'+data.id_room).clients((error, clients) => {
-            if (error)  {
-              console.log('Error: connection cliente');
-              console.log(err);
-            } else{
+})
 
-              participantes = clients.length
-              resultados = []
-              if(clients.length > 0){
-                var sql =  " SELECT * FROM( SELECT comunicacionDtRoomMensajes.id_users_envio AS 'user', "
-                    sql += "    comunicacionDtRoomMensajes.id_comunicacionCtRoom AS 'id_room',";
-                    sql += "    users.name AS 'username',"; 
-                    sql += "    comunicacionDtRoomMensajes.mensaje AS 'message',";
-                    sql += "     DATE_FORMAT(comunicacionDtRoomMensajes.created_at,  '%H %i  %W %M %e %Y') AS 'fecha',"; 
-                    sql += "    comunicacionDtRoomMensajes.created_at AS 'DATE'" 
-                    sql += " FROM comunicacionDtRoomMensajes "; 
-                    sql += " INNER JOIN users ON users.id =  comunicacionDtRoomMensajes.id_users_envio ";
-                    sql += " WHERE comunicacionDtRoomMensajes.id_comunicacionCtRoom = ? AND comunicacionDtRoomMensajes.state = 'A' Order By comunicacionDtRoomMensajes.created_at DESC LIMIT 10 ";   
-                    sql += " ) sub ORDER BY DATE ASC;"
-                mensajero.query(sql, [id_room], function (err, result) {
-                  if (err){
-                    console.log(err)
-                  } else
-                    resultados = result;
-                  socket.emit('login', {
-                    username: data.username,
-                    id_origen: data.id_origen,
-                    id_room:  id_room,
-                    resultados: resultados
+promise
+  .then( (resolve, reject) => {
+    console.log('Conexion DB correcta.')
+    /*************************************** */
+    io.on('connection', (socket) => {
+      var item = {},
+          _id_doom = "",
+          _user = "",
+          _username = "";
+  
+      socket.on('new message', (data) => {
+        // we tell the client to execute 'new message'
+        socket.to('chat-'+data.id_room).emit('new message', {//socket.broadcast.emit('new message', {//
+          username: data.username,
+          message:  data.message, 
+          id_room:  data.id_room, 
+          user:     data.user, 
+        });
+        var sql = "INSERT INTO comunicacionDtRoomMensajes (mensaje,state, id_comunicacionCtRoom, id_users_envio) VALUES ?";
+        var values = [
+          [data.message, 'A',data.id_room,data.user]
+        ];
+        resolve.query(sql, [values], function (err, result) {
+          if (err){
+            console.log(err);
+          } 
+        });
+  
+      });
+    
+      // when the client emits 'add user', this listens and executes
+      socket.on('add user', (data) => {
+        socket.join('chat-'+data.id_room, () => {
+          _id_doom  = data.id_room;
+          _user     = data.id_origen;
+          _username = data.username;
+          let resultados = [];
+    
+        let id_room = data.id_room;
+        let participantes = 0//addRoom(data.id_room)
+       
+            io.in('chat-'+data.id_room).clients((error, clients) => {
+              if (error)  {
+                console.log('Error: connection cliente: '.data.id_room);
+                console.log(err);
+              } else{
+                
+                participantes = clients.length
+                resultados = []
+                //console.log(participantes)
+                if(clients.length > 0){
+                  var sql =  " SELECT * FROM( SELECT comunicacionDtRoomMensajes.id_users_envio AS 'user', "
+                      sql += "    comunicacionDtRoomMensajes.id_comunicacionCtRoom AS 'id_room',";
+                      sql += "    users.name AS 'username',"; 
+                      sql += "    comunicacionDtRoomMensajes.mensaje AS 'message',";
+                      sql += "     DATE_FORMAT(comunicacionDtRoomMensajes.created_at,  '%H %i  %W %M %e %Y') AS 'fecha',"; 
+                      sql += "    comunicacionDtRoomMensajes.created_at AS 'DATE'" 
+                      sql += " FROM comunicacionDtRoomMensajes "; 
+                      sql += " INNER JOIN users ON users.id =  comunicacionDtRoomMensajes.id_users_envio ";
+                      sql += " WHERE comunicacionDtRoomMensajes.id_comunicacionCtRoom = ? AND comunicacionDtRoomMensajes.state = 'A' Order By comunicacionDtRoomMensajes.created_at DESC LIMIT 10 ";   
+                      sql += " ) sub ORDER BY DATE ASC;"
+                    resolve.query(sql, [id_room], function (err, result) {
+                    if (err){
+                      console.log(err)
+                    } else{
+                   
+                      socket.emit('login', {
+                        username: data.username,
+                        id_origen: data.id_origen,
+                        id_room:  id_room,
+                        resultados: result
+                      });
+                    }
                   });
-                });
-                if(clients.length > 1){
-                  io.to('chat-'+data.id_room).emit('user joined', {
-                    username: data.username,
-                    id_origen: data.id_origen,
-                    id_room:  id_room
-                  });
+
+                  exports.executeQuery=function(query,callback){
+                    pool.getConnection(function(err,connection){
+                        if (err) {
+                          connection.release();
+                          throw err;
+                        }   
+                        connection.query(sql, [id_room],function(err,rows){
+                            connection.release();
+                            if(!err) {
+                                callback(null, {rows: rows});
+                            }           
+                        });
+                        connection.on('error', function(err) {      
+                              throw err;
+                              return;     
+                        });
+                    });
+                }
+
+
+
+
+                  if(clients.length > 1){
+                    io.to('chat-'+data.id_room).emit('user joined', {
+                      username: data.username,
+                      id_origen: data.id_origen,
+                      id_room:  id_room
+                    });
+                  }
                 }
               }
-            }
-            
-          });
-       });
-    });
-  
-    // when the client emits 'typing', we broadcast it to others
-    socket.on('typing', (data) => {
-       socket.to(data.grupos).emit('typing', {
-        username: socket.username
+              
+            });
+         });
       });
-    });
-  
-    // when the client emits 'stop typing', we broadcast it to others
-    socket.on('stop typing', (data) => {
-      socket.to(data.grupos).emit('stop typing', {
-        username: socket.username
+    
+      // when the client emits 'typing', we broadcast it to others
+      socket.on('typing', (data) => {
+         socket.to(data.grupos).emit('typing', {
+          username: socket.username
+        });
       });
-    });
-
-    socket.on('disconnect', (reason) => {  
-      socket.to('chat-'+_id_doom).emit('user left', {
-        username: _username,
-        id_origen:_user,
-        id_room:  _id_doom
+    
+      // when the client emits 'stop typing', we broadcast it to others
+      socket.on('stop typing', (data) => {
+        socket.to(data.grupos).emit('stop typing', {
+          username: socket.username
+        });
       });
+  
+      socket.on('disconnect', (reason) => {  
+        socket.to('chat-'+_id_doom).emit('user left', {
+          username: _username,
+          id_origen:_user,
+          id_room:  _id_doom
+        });
+      });
+    
     });
-  
-  });
-  
+    
+    /**************************************** */
+    /**************************************** */
+  })
+  .catch( (err) => {} )
